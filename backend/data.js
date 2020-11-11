408c2eb28f585b759edef3a9c467eff947ed5c51
pgoytia <pgoytia@gmail.com>
2020-11-11 16:18:06 -0600

    primera subida2

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+export default  {
+    products:[
+        {
+
+        _id:'1',
+        name:'Sueter',
+        category:'Prendas',
+        image:'/images/d1.jpg',
+        price:'1399',
+        brand:'BALMAIN',
+        raiting:4.5,
+        numReviews:10,
+        countInStock:0,
+        },
+        {
+
+            _id:'2',
+            name:'Sueter',
+            category:'Prendas',
+            image:'/images/d2.jpg',
+            price:'399',
+            brand:'GUCCI',
+            raiting:5.0,
+            numReviews:15,
+            countInStock:5,
+            },
+            {
+
+                _id:'3',
+                name:'Bota MArtens',
+                category:'Calzado',
+                image:'/images/d3.jpg',
+                price:'199',
+                brand:'Fendi',
+                raiting:4.0,
+                numReviews:6,
+                countInStock:4,
+                },
+        
+    ]
+
+}
